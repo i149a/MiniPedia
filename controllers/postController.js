2ed398a17aff938e4766a371b90a8ea2e76a5c8e
Israa <israa.wedaei@gmail.com>
2025-05-20 17:12:26 +0300

    Post model updated

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,84 +1,165 @@
-const User = require('../models/User.js')  // Accessing User model
-const Post = require('../models/Post.js')  // Accessing Post model
+// Import required models
+const User = require('../models/User.js')
+const Post = require('../models/Post.js')
+const Tag = require('../models/Tag.js')
 
-// Creating new post function 
+
+// CREATE POST FUNCTION
 const createPost = async (req, res) => {
     try {
-        // Find the user that is associated with this post
-        const user = await User.findById(req.body.author) // author is who created the post
+        const { title, body, image, tags = [] } = req.body
+        const author = req.body.author // Current user's ID (Who is writing the post)
+        
+        // Create new post
+        const post = await Post.create({
+            title,
+            body,
+            image, 
+            author,
+            tags
+        })
 
-        // Create the post
-        const post = await Post.create(req.body)
+        // Add post to author's posts array
+        await User.findByIdAndUpdate(author, {
+            $push: { posts: post._id } // $push adds to array
+        })
 
-        // Update our user in the database by adding our new post's ObjectID to their array    
-        user.posts.push(post._id)
-        user.save()
+        // Add post to all related tags
+        await Tag.updateMany(
+            { _id: { $in: tags } }, // Find all tags in the tags array
+            { $push: { posts: post._id } } // Add post ID to each tag's posts array
+        )
 
-        // Sending a respnse to verify that the flower is created
+        // Redirect to the new post's page
         res.redirect(`/posts/${post._id}`)
     } catch (error) {
-        console.error('An error has occurred creating a post!', error.message) // To handle the errors
+        console.error('Error creating post:', error.message)
+        res.status(500).send('Error creating post')
     }
 }
 
-// Get all posts function
+// GET ALL POSTS FUNCTION
 const getAllPosts = async (req, res) => {
     try {
-        // Query for posts collection 
+        // Find all posts and populate related data
         const posts = await Post.find({})
-
-        // Sending a respnse to verify
-        res.render('./posts/all.ejs', { posts })
+            .populate('author', 'username')
+            .populate('tags', 'title')
+            
+        // Render view with posts and current user data
+        res.render('./posts/all.ejs', { 
+            posts,
+            currentUser: req.user // Pass logged-in user data
+        })
     } catch (error) {
-        console.error('An error has occurred getting all posts!', error.message) // To handle the errors
+        console.error('Error getting posts:', error.message)
+        res.status(500).send('Error getting posts')
     }
 }
 
-// Get a post by ID function
+// GET A POST BY ID FUNCTION
 const getPostById = async (req, res) => {
     try {
-        // Query for a specific post ID
+        // Find post and populate all relationships
         const post = await Post.findById(req.params.id)
-
-        // Sending a respnse to verify
-        res.render('./posts/show.ejs', { post })
+            .populate('author', 'username')
+            .populate('editors', 'username')
+            .populate('tags', 'title description')
+            
+        // Get all available tags for tag selection
+        const allTags = await Tag.find({})
+        
+        // Render single post view with all data
+        res.render('./posts/show.ejs', { 
+            post,
+            allTags,
+            currentUser: req.user
+        })
     } catch (error) {
-        console.error('An error has occurred getting a post!', error.message) // To handle the errors
+        console.error('Error getting post:', error.message)
+        res.status(500).send('Error getting post')
     }
 }
 
-// Uodate a specific post by ID
+// UPDATE POST BY ID FUNCTION 
 const updatePostById = async (req, res) => {
     try {
-        // Find and Update the post using the id from the params
-        // Passing the updated fields
-        const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true }) // { new: true } ensures that the updated record is what is returned
-
-        // Sending a respnse to verify
+        const { title, body, image, editor, tags = [] } = req.body
+        
+        // Get current post data for comparison
+        const currentPost = await Post.findById(req.params.id)
+        const currentTags = currentPost.tags.map(tag => tag.toString())
+        
+        // Determine which tags were added/removed
+        const tagsToAdd = tags.filter(tag => !currentTags.includes(tag))
+        const tagsToRemove = currentTags.filter(tag => !tags.includes(tag))
+        
+        // Update the post
+        const post = await Post.findByIdAndUpdate(
+            req.params.id,
+            {
+                title,
+                body,
+                image,
+                edited: true, // Mark as edited
+                $addToSet: { editors: editor }, // Add editor if not already present
+                tags // Set new tags array
+            },
+            { new: true } // Return the updated document
+        )
+        
+        // Add post to new tags
+        await Tag.updateMany(
+            { _id: { $in: tagsToAdd } },
+            { $addToSet: { posts: post._id } } // $addToSet prevents duplicates
+        )
+        
+        // Remove post from removed tags
+        await Tag.updateMany(
+            { _id: { $in: tagsToRemove } },
+            { $pull: { posts: post._id } } // $pull removes from array
+        )
+        
+        // Redirect to updated post
         res.redirect(`/posts/${post._id}`)
     } catch (error) {
-        console.error('An error has occurred updating a post!', error.message) // To handle the errors
+        console.error('Error updating post:', error.message)
+        res.status(500).send('Error updating post')
     }
 }
 
-// Delete post by ID
+// DELETE POST BY ID FUNCTION
 const deletePostById = async (req, res) => {
     try {
-        // Find and Delete the post using the id from the params
-        await Post.findByIdAndDelete(req.params.id) // No need to store this in a variable since it's being deleted
-
-        // Sending a respnse to verify 
-        res.render('./posts/confirm.ejs')
+        // Find and delete the post
+        const post = await Post.findByIdAndDelete(req.params.id)
+        
+        // Remove post from author's posts array
+        await User.findByIdAndUpdate(post.author, {
+            $pull: { posts: post._id } // Remove post ID
+        })
+        
+        // Remove post from all related tags
+        await Tag.updateMany(
+            { posts: post._id },
+            { $pull: { posts: post._id } }
+        )
+        
+        // Render deletion confirmation page
+        res.render('./posts/confirm.ejs', { 
+            currentUser: req.user // Pass user data for header
+        })
     } catch (error) {
-        console.error('An error has occurred deleting a post!', error.message) // To handle the errors
+        console.error('Error deleting post:', error.message)
+        res.status(500).send('Error deleting post')
     }
 }
 
-// To export All functions that are made
+// Export all functions
 module.exports = {
-  createPost,
-  getAllPosts,
-  getPostById,
-  updatePostById,
-  deletePostById
+    createPost,
+    getAllPosts,
+    getPostById,
+    updatePostById,
+    deletePostById
 }
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,33 +1,45 @@
-const express = require('express') // Import Express
-const router = express.Router() // Set up the router object 
-const postController = require('../controllers/postController.js') // Import post controller
-const Post = require('../models/Post.js') // Import post router
+const express = require('express')
+const router = express.Router()
+const postController = require('../controllers/postController.js')
+const Post = require('../models/Post.js')
+const Tag = require('../models/Tag.js')
 
-// POST method for creating a post for specific user (router, imported controller.function)
+// Create Post 
 router.post('/', postController.createPost)
 
-// GET method for getting all posts (router, imported controller.function)
+// Get All Posts 
 router.get('/', postController.getAllPosts)
 
 // Rendering new post page
-router.get('/new', (req, res) => {
-  res.render('./posts/new.ejs')
+router.get('/new', async (req, res) => {
+  const tags = await Tag.find({}).select('title')
+  res.render('./posts/new.ejs', { tags })
 })
 
-// GET method for getting a post by ID (router, imported controller.function)
+// Get Post By ID
 router.get('/:id', postController.getPostById)
 
-// PUT method for updating a specific post (router, imported controller.function)
+// Update Post By ID
 router.put('/:id', postController.updatePostById)
 
-// DELETE method for deleting a specific post (router, imported controller.function)
+// Delete Post By ID
 router.delete('/:id', postController.deletePostById)
 
-// Rendering edit post page
+// Edit Post By ID
 router.get('/:id/edit', async (req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render('./posts/edit.ejs', { post })
+  const [post, tags] = await Promise.all([
+    Post.findById(req.params.id),
+    Tag.find({}).select('title')
+  ])
+  
+  // Checks is the post can be edited or not
+  if (!post.editable)
+    return res.redirect('/posts')
+    
+  res.render('./posts/edit.ejs', { 
+    post,
+    tags
+  })
 })
 
-// Export the router
 module.exports = router
